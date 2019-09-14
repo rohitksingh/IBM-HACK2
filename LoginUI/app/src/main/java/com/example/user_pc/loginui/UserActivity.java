c28438f98dab2d4194af205cc39d6847b5ed9066
Rohit <r4rohit002@gmail.com>
2019-09-14 11:01:17 -0700

    Added Logo and Name

--- LoginUI/app/src/main/java/com/example/user_pc/loginui/UserActivity.java
+++ LoginUI/app/src/main/java/com/example/user_pc/loginui/UserActivity.java
@@ -3,12 +3,14 @@ package com.example.user_pc.loginui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class UserActivity extends AppCompatActivity {
 
     private Button myInfo, yourInfo, see;
+    private static final String TAG = "UserActivity";
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -38,6 +40,16 @@ public class UserActivity extends AppCompatActivity {
                 reachOut();
             }
         });
+
+        String extraMsg = getIntent().getStringExtra("click_action");
+        if(extraMsg!=null) {
+            Log.d(TAG, "onCreate: "+extraMsg);
+        }else
+        {
+            Log.d(TAG, "onCreate: is Null");
+        }
+
+
 
 
     }
